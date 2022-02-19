c0f616fb328e7ae5d67598bcb5d3dc434a70ec38
sipocz <sipoczlaszlo@gmail.com>
2022-02-19 13:27:59 +0100

    20220219132700
    search box

--- Flask/server.py
+++ Flask/server.py
@@ -15,29 +15,8 @@ _AI_Search_Engine_URL_="http://192.168.2.6:5001/query/virus%20database%20update%
 req = requests.get(_AI_Search_Engine_URL_)
 txt=req.text
 print(txt)
-output=[
- '  0 .. 93.4% .. 3BSE078160 en R ABB Ability System 800xA 6.0.3.3 Product Catalog.pdf                                                     .. page:  53',
- '  1 .. 89.1% .. 3BSE035982-610_A_en_Control_AC_800M_Communication_Protocols.pdf                                                          .. page: 206',
- '  2 .. 88.8% .. 3BSE035981-600_A_en_System 800xA_Control_6.0_AC_800M_Binary_and_Analog_Handling.pdf                                      .. page:   5',
- '  3 .. 88.7% .. 3BSE035981-600_en_System 800xA_Control_6.0_AC_800M_Binary_and_Analog_Handling.pdf                                        .. page:   5',
- '  4 .. 88.5% .. 3BSE035981-600_en_System 800xA_Control_6.0_AC_800M_Binary_and_Analog_Handling.pdf                                        .. page:  78',
- '  5 .. 88.5% .. 3BSE038018-600_en_System_800xA_6.0_System_Guide_Functional_Description.pdf                                               .. page: 149',
- '  6 .. 88.2% .. 3BSE041488-511_-_en_Compact_Control_Builder_AC_800M_5.1.1_Binary_and_Analog_Handling.pdf                                 .. page:   5',
- '  7 .. 88.1% .. 3BSE025251-600_en_S800_I_O_CI840_Memory_Maps.pdf                                                                         .. page: 197',
- '  8 .. 88.0% .. 3BSE038018-510_H_en_System_800xA_5.1_System_Guide_Functional_Description.pdf                                             .. page: 172',
- '  9 .. 87.9% .. 2PAA112277-603_en_System_800xA_6.0_Release_Notes_Resolved_Issues.pdf                                                     .. page: 165',
- ' 10 .. 87.9% .. 3BSE035981-510_A_en_System_800xA_Control_5.1_AC_800M_Binary_and_Analog.pdf                                               .. page:  83',
- ' 11 .. 87.9% .. 3BDD013138R0001_-_en_System_800xA__AC_870P_-_Local_I_O.pdf                                                               .. page:   2',
- ' 12 .. 87.8% .. 3BSE082729_B_en_Select_IO_FCI_Ethernet.pdf                                                                               .. page:  62',
- ' 13 .. 87.8% .. 3BSE041488-600_A_en_Compact_Control_Builder_AC_800M_6.0_Binary_and_Analog_Handling.pdf                                   .. page:  58',
- ' 14 .. 87.7% .. 2PAA114363-510_B_en_System_800xA_5.1_RevE_Release_Notes_New_Functions_and_Known_Problems.pdf                             .. page: 147',
- ' 15 .. 87.7% .. 3BSE036959-600_en_S800_I_O_CI801_Memory_Maps.pdf                                                                         .. page: 181',
- ' 16 .. 87.7% .. 3BSE035981-510_A_en_System_800xA_Control_5.1_AC_800M_Binary_and_Analog.pdf                                               .. page:   6',
- ' 17 .. 87.7% .. 3BSE038018-510_H_en_System_800xA_5.1_System_Guide_Functional_Description.pdf                                             .. page:  16',
- ' 18 .. 87.6% .. 3BSE041488-511_-_en_Compact_Control_Builder_AC_800M_5.1.1_Binary_and_Analog_Handling.pdf                                 .. page:  55',
- ' 19 .. 87.6% .. ABB UNIVERSITY INDIA Course Catalog.pdf                                                                                  .. page:   3']
 
-print(output[0])
+
 
 @app.route('/')
 def hello_world():
@@ -66,9 +45,11 @@ def query2():
 
     req_list=list(req_dict.values())
     print(req_list)
+    query_txt=_query
     if processed_text!="aaa":
         outstr=render_template("query.html",
-                                 _query=req_list
+                                 _query=req_list,
+                                 _query_text=query_txt
                                  )
     
     return outstr
--- Flask/static/js/main.js
+++ Flask/static/js/main.js
@@ -1,13 +1,34 @@
-const input = document.getElementById("checkbox")
+let darkMode = localStorage.getItem('darkMode')
+const darkModeToggle = document.querySelector('#button')
 
-document.body.setAttribute("data-theme", "light")
+const darkmodeIcon = "bi bi-moon"
+const lightmodeIcon = "bi bi-brightness-high-fill"
+const icon = document.getElementById("switcher");
+icon.className = lightmodeIcon;
 
-input.addEventListener('change', (e) =>{
-    if(e.target.checked){
-        document.body.setAttribute('data-theme', 'dark')
+
+const enableDarkMode = () =>{
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkMode', 'enabled')
+    icon.className=darkmodeIcon
+}
+
+const disableDarkMode = () =>{
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkMode', null)
+    icon.className=lightmodeIcon;
+}
+
+if(darkMode === "enabled"){
+    enableDarkMode()
+}
+
+darkModeToggle.addEventListener("click", ()=>{
+    darkMode = localStorage.getItem('darkMode')
+    if(darkMode !== 'enabled'){
+        enableDarkMode()
     }
-
     else{
-        document.body.setAttribute('data-theme', 'light')
+        disableDarkMode()
     }
 })
