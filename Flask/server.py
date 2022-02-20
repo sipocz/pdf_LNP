3e2861c26296c0a87613881e69e76ae1da7d8031
sipocz <sipoczlaszlo@gmail.com>
2022-02-20 13:39:46 +0100

    20220220133900

--- Flask/server.py
+++ Flask/server.py
@@ -1,5 +1,5 @@
 from asyncio.windows_events import NULL
-from flask import Flask
+from flask import Flask, send_from_directory, send_file
 from flask import render_template_string
 from flask import render_template,request
 from werkzeug.utils import secure_filename
@@ -8,7 +8,7 @@ import pymongo
 from collections import Counter
 
 import os
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static")
 
 import requests
 
@@ -22,27 +22,38 @@ def statistic(inp:list,best_n=3):
     cc=Counter(fname_list)
     return cc.most_common(best_n)
 
+
+
+
 def get_mongo_fileurl(fname:str):
     '''
     MONGODB adatbázisból filename alapján url visszaadása
     '''
+    print("Mongo_start")
     _PDF_DB_="PDF_DB"
     _FILE_LOCATION_COLLECTION_="ABB_file_location"
     client = pymongo.MongoClient("mongodb+srv://pdfaidata:pdfaidatapwd@cluster0.fuant.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     mydb = client[_PDF_DB_]
     col=mydb[_FILE_LOCATION_COLLECTION_]
+
+
     cursor=col.find({"fname":fname})
     out_list=[]
     for out in cursor:
         out_list.append(out)
+    print("Mongo end")
     return(out_list)
 
 print("**** MONGODB  ****")  # DEBUG
 print(get_mongo_fileurl("3BSE041434-600_A_en_System_800xA_6.0_System_Guide_Technical_Data_and_Configuration.pdf")) # DEBUG
 
 
-
-
+def path_splitter(url):
+    import pathlib
+    split_up = url.split("/")
+    out=[i+"/" for i in split_up[:-1]  ]
+    out="".join(out) 
+    return(out)
 
 
 @app.route('/')
@@ -52,13 +63,22 @@ def hello_world():
     return outstr
 
 
+@app.route('/download/<fname>')
+def download(fname):
+    print("-------------- DOWNLOAD --------------")
+    print(f"fname:{fname}")
+    
+    print(f"PATH:{app.instance_path}")
+    ret=get_mongo_fileurl(fname)
+    print(ret)
+    url=ret[0]["url"]
+    directory=path_splitter(url)
+    print(f"directory:{directory}")
+    return send_file(directory+fname+".pdf",as_attachment=False)
 
 
 
 @app.route('/query_str', methods=['POST'])
-
-
-
 def query2():
     _query=request.form["query_str"]
     print(_query)
@@ -89,6 +109,7 @@ def query2():
 
     print("++++++ REQ_ LIST +++++++++")
     print(req_list)
+
     for req_list_item in req_list:
         req_list_item["url"]=get_mongo_fileurl(req_list_item["fname"])[0]["url"]
 
